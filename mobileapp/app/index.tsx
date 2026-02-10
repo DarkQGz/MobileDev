92ec27c112d7b2656a3487e4b2ef3e2735e96836
svhe4u <143775419+Temuulenino@users.noreply.github.com>
2026-02-10 12:16:56 +0800

    Update index.tsx

--- mobileapp/app/index.tsx
+++ mobileapp/app/index.tsx
@@ -5,7 +5,6 @@ import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
 import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
 
 export default function Index() {
-  // Array of icons corresponding to each category
   const icons = [
     <FontAwesome5 name="wolf-pack-battalion" size={40} color="black" />,
     <MaterialCommunityIcons name="food-turkey" size={40} color="black" />,
@@ -20,23 +19,11 @@ export default function Index() {
         keyExtractor={(item) => item.cid.toString()}
         renderItem={({ item, index }) => (
           <View style={styles.card}>
-            {/* ICON ABOVE TITLE */}
-            <View style={styles.iconContainer}>
-              {icons[index] /* Select icon based on category index */}
-            </View>
-
-            {/* CATEGORY TITLE */}
+            <View style={styles.iconContainer}>{icons[index]}</View>
             <Text style={styles.title}>{item.cname}</Text>
-
-            {/* SUBCATEGORIES */}
-            {item.subcat.map((sub) => (
-              <View key={sub.sid} style={styles.row}>
-                <Text style={styles.sub}>{sub.sname}</Text>
-                <Text style={styles.count}>{sub.item.length}</Text>
-              </View>
-            ))}
           </View>
         )}
+        showsHorizontalScrollIndicator={false}
       />
     </View>
   );
@@ -48,9 +35,9 @@ const styles = StyleSheet.create({
     paddingLeft: 12,
   },
   card: {
-    backgroundColor: "#f2f2f2", // light gray
+    backgroundColor: "#f2f2f2",
     borderRadius: 12,
-    padding: 12,
+    padding: 20,
     marginRight: 12,
     minWidth: 220,
     alignItems: "center",
@@ -61,22 +48,6 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 18,
     fontWeight: "bold",
-    marginBottom: 8,
     textAlign: "center",
-  },
-  row: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    paddingVertical: 6,
-    borderBottomWidth: 1,
-    borderBottomColor: "#ddd",
-    width: "100%",
-  },
-  sub: {
-    fontSize: 14,
-  },
-  count: {
-    fontSize: 14,
-    fontWeight: "600",
   },
 });
