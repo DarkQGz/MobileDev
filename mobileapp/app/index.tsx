93fd4f32bc4e3693c71e879fa3955978527f4ca1
svhe4u <143775419+Temuulenino@users.noreply.github.com>
2026-02-10 12:03:51 +0800

    Update index.tsx

--- mobileapp/app/index.tsx
+++ mobileapp/app/index.tsx
@@ -1,18 +1,34 @@
 import React from "react";
 import { View, Text, FlatList, StyleSheet } from "react-native";
 import babyData from "./data/babydata";
+import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
 
 export default function Index() {
+  // Array of icons corresponding to each category
+  const icons = [
+    <FontAwesome5 name="wolf-pack-battalion" size={40} color="black" />,
+    <MaterialCommunityIcons name="food-turkey" size={40} color="black" />,
+    <FontAwesome5 name="car-crash" size={40} color="black" />,
+  ];
+
   return (
     <View style={styles.container}>
       <FlatList
         data={babyData}
         horizontal
         keyExtractor={(item) => item.cid.toString()}
-        renderItem={({ item }) => (
+        renderItem={({ item, index }) => (
           <View style={styles.card}>
+            {/* ICON ABOVE TITLE */}
+            <View style={styles.iconContainer}>
+              {icons[index] /* Select icon based on category index */}
+            </View>
+
+            {/* CATEGORY TITLE */}
             <Text style={styles.title}>{item.cname}</Text>
 
+            {/* SUBCATEGORIES */}
             {item.subcat.map((sub) => (
               <View key={sub.sid} style={styles.row}>
                 <Text style={styles.sub}>{sub.sname}</Text>
@@ -26,7 +42,6 @@ export default function Index() {
   );
 }
 
-
 const styles = StyleSheet.create({
   container: {
     marginTop: 50,
@@ -38,6 +53,10 @@ const styles = StyleSheet.create({
     padding: 12,
     marginRight: 12,
     minWidth: 220,
+    alignItems: "center",
+  },
+  iconContainer: {
+    marginBottom: 8,
   },
   title: {
     fontSize: 18,
@@ -51,6 +70,7 @@ const styles = StyleSheet.create({
     paddingVertical: 6,
     borderBottomWidth: 1,
     borderBottomColor: "#ddd",
+    width: "100%",
   },
   sub: {
     fontSize: 14,
